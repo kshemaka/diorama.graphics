02ca3f9340d833cca179ff8470f7c02a1405ffda
Asheesh Gulati <kshemaka@users.noreply.github.com>
2023-09-09 04:46:00 +0200

    Integrate the changes in kshemaka/diorama-js@81e5031
    The canvas resizes to fill the visual viewport of the browser. Its actual resolution is set to a maximum of 400 x 400 pixels.

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,53 @@
 <script setup>
-import { nextTick, onBeforeUnmount, onMounted, ref } from "vue"
+import { computed, nextTick, onBeforeUnmount, onMounted, ref, watch } from "vue"
 import { initializeDiorama } from "@kshemaka/diorama"
 
 import scene from "./assets/spheres.x3d?raw"
 
+const debounce = function (fn, t) {
+    let timer
+    return function (...args) {
+        clearTimeout(timer)
+        timer = setTimeout(() => fn(...args), t)
+    }
+}
+
 const canvas = ref(null)
+
 let diorama = null
+const width = ref(window.innerWidth)
+
+const height = ref(window.innerHeight)
+const style = computed(() => {
+    return {
+        width: width.value + "px",
+        height: height.value + "px"
+    }
+})
+const dimensions = computed(() => {
+    return {
+        width: width.value,
+        height: height.value
+    }
+})
+watch(dimensions, () => {
+    diorama.resizeCanvas(dimensions.value)
+})
+const updateDimensions = debounce((w, h) => {
+    width.value = w
+    height.value = h
+}, 100)
 
 onMounted(() => {
     if (canvas.value.getContext) {
         const c = canvas.value
-        c.width = 400
-        c.height = 400
+        c.width = 400 // max canvas width
+        c.height = 400 // max canvas height
         diorama = initializeDiorama(c)
         diorama.loadScene(scene)
 
         nextTick(() => {
+            window.addEventListener("resize", onResize)
             window.addEventListener("keydown", onKeyDown)
             window.addEventListener("keyup", onKeyUp)
         })
@@ -27,7 +59,12 @@ onMounted(() => {
 onBeforeUnmount(() => {
     window.removeEventListener("keyup", onKeyUp)
     window.removeEventListener("keydown", onKeyDown)
+    window.removeEventListener("resize", onResize)
 })
+
+function onResize() {
+    updateDimensions(window.innerWidth, window.innerHeight)
+}
 
 function onKeyDown(event) {
     diorama.triggerAction(event.key, true)
@@ -39,7 +76,8 @@ function onKeyUp(event) {
 </script>
 
 <template>
-    <canvas ref="canvas">
+    <canvas ref="canvas" :style="style">
+        <!-- <canvas ref="canvas"> -->
         <!-- TODO ensure that fallback content is provided for accessibility -->
     </canvas>
 </template>
@@ -47,7 +85,7 @@ function onKeyUp(event) {
 <style scoped>
 canvas {
     display: block;
-    margin: auto;
-    border: 1px solid black;
+    width: 100%;
+    height: 100%;
 }
 </style>
